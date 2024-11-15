99d11123209c4a7f388ebd2028b332b782e7e45a
TresTristesTrigues <spnispni9@gmail.com>
2024-11-15 14:58:57 +0000

    neovide font

--- init.lua
+++ init.lua
@@ -4,6 +4,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+if vim.g.neovide then
+  vim.o.guifont = 'FiraCode Nerd Font:h12'
+end
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
