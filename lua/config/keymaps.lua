39b15b49c7ef0f55d68adddebdd2952eb1cca373
Spin <>
2025-03-20 19:03:50 +0000

    Updated plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>e", "<cmd>Neotree<CR>", { desc = "Explorer Neotree" })
-vim.keymap.set("n", "<leader>o", "<cmd>Neotree toggle<CR>", { desc = "Explorer Neotree (Toggle Focus)" })
